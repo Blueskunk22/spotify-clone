619376d0bf6eb2e7ab5d24b432d14e57309c63b5
Blueskunk22 <jakob.hauble@gmail.com>
2022-02-05 15:56:53 -0600

    Update title and favicon

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { getSession } from "next-auth/react";
+import Head from "next/head";
 import Center from "../components/Center";
 import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
@@ -6,6 +7,10 @@ import Sidebar from "../components/Sidebar";
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify Clone - Hobbes</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <main className="flex">
         <Sidebar />
         <Center />
